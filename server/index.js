e81afd1b8c99492fa9421757957b8e61eb6244f4
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-17 17:17:33 -0700

    add RESTful CRUD routes feature

--- server/index.js
+++ server/index.js
@@ -17,30 +17,135 @@ app.listen(port, () => {
   console.log(`Server is now listening on port: ${port}`)
 })
 
+
+app.get('/:stockId', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../public/dist/index.html'));
+})
+
+
+// READ
+
 app.get('/api/:stockId', (req, res) => {
-  console.log('Got a request searching for', req.params.stockId);
-  Stocks.find({stockId: req.params.stockId}, (err, data) => {
+  console.log('Got a request to READ at ', req.params.stockId);
+  var id = req.params.stockId;
+  Stocks.find({stockId: id}, (err, data) => {
     if (err) {
       console.log(err.message);
     } else if (!data.length) {
-      Stocks.find({id: req.params.stockId}, (err, data) => {
+      Stocks.find({id: id}, (err, data) => {
         if (err) {
           console.log(err.message);
         } else if (!data.length) {
           console.log('Data not found');
           res.sendStatus(404);
         } else {
-          console.log(`Sending ${req.params.stockId} to client`);
+          console.log(`Sending ${id} to client`);
+          console.log(data);
           res.send(data);
         }
       }) 
     } else {
-      console.log(`Sending ${req.params.stockId} to client`);
+      console.log(`Sending ${id} to client`);
+      console.log(data);
       res.send(data);
     }
   }) 
 })
 
-app.get('/:stockId', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../public/dist/index.html'));
+// CREATE
+
+app.post('/api/:stockId', (req, res) => {
+  console.log('Got a request to POST at ', req.params.stockId);
+  var newStock = req.body.data;
+  Stocks.create({ 
+    "stockInfo" : { 
+    "relatedTags" : newStock.relatedTags, 
+    "stockCompany" : newStock.stockCompany, 
+    "noOfOwners" : newStock.noOfOwners, 
+    "recommendationPercent" : newStock.recommendationPercent
+  }, 
+  "stockData" : { 
+    "day" : newStock.dayData, 
+    "week" : newStock.weekData, 
+    "month" : newStock.monthData, 
+    "threeMonth" : newStock.threeMonthData, 
+    "year" : newStock.yearData, 
+    "fiveYear" : newStock.fiveYearData 
+    }, 
+    "id" : newStock.id, 
+    "stockId" : newStock.stockId, 
+    "averageStock" : newStock.averageStock, 
+    "changePercent" : newStock.changePercent, 
+    "createdAt" : "2019-04-17T17:58:57.498Z", 
+    "updatedAt" : "2019-04-17T17:58:57.498Z", 
+  }, (error) => {
+    if (error){
+      res.statusCode(500);
+      res.end();
+    } else {
+      console.log('POST was success')
+    }
+  });
+})
+
+// UPDATE
+
+app.patch('/api/:stockId', function(req, res) {
+  console.log('Got a request to UPDATE at ', req.params.stockId);
+  var id = req.params.stockId;
+  var newStock = req.body.data;
+  Stocks.find({ id: id }, (error) => {
+    if (error) {
+      res.statusCode(500);
+      res.end();
+    } else {
+      Stocks.findOneAndUpdate({ id: id }, {$set: { 
+        "stockInfo" : { 
+        "relatedTags" : newStock.relatedTags, 
+        "stockCompany" : newStock.stockCompany, 
+        "noOfOwners" : newStock.noOfOwners, 
+        "recommendationPercent" : newStock.recommendationPercent
+      }, 
+      "stockData" : { 
+        "day" : newStock.dayData, 
+        "week" : newStock.weekData, 
+        "month" : newStock.monthData, 
+        "threeMonth" : newStock.threeMonthData, 
+        "year" : newStock.yearData, 
+        "fiveYear" : newStock.fiveYearData 
+        }, 
+        "id" : newStock.id, 
+        "stockId" : newStock.stockId, 
+        "averageStock" : newStock.averageStock, 
+        "changePercent" : newStock.changePercent, 
+        "createdAt" : "2019-04-17T17:58:57.498Z", 
+        "updatedAt" : "2019-04-17T17:58:57.498Z", 
+      }
+    }, {}, (err) => {
+        if (err) {
+          res.statusCode(500);
+          res.end();
+        } else {
+          console.log('UPDATE was success');
+          res.statusCode(200);
+        }
+      });
+    }
+  });
+});
+
+
+// DELETE
+
+app.delete('/api/:stockId', function (req, res) {
+  var id = req.params.stockId;
+  Stocks.deleteOne({ id: id }, (error) => {
+    if (error){
+      res.statusCode(500);
+      res.end();
+    } else {
+      console.log('DELETE was success');
+      res.statusCode(200);
+    }
+  })
 })
