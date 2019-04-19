15b97ac6d8bb9752797ef01ed71534e2318ce2b0
Anthony Riggi <anthonyriggithree@gmail.com>
2019-04-19 14:45:51 -0700

    implement seed using drainage with output to a csv

--- database/seed.js
+++ database/seed.js
@@ -1,23 +1,43 @@
+const fs = require('fs');
 const db  = require('./index.js');
 const StockChart = require('./StockChart.js');
 const generateData = require('./generateData.js');
 
-const stockData = [];
-for (let i = 0; i < 100; i++) {
-  // stockData.push(generateData.generateData(i));
-  process.stdout.write(JSON.stringify(generateData.generateData(i)));
+function writeTenMillionTimes(writer, data) {
+ return new Promise((resolve) => {
+   if (writer.write(data)) {
+     writer.once('drain', resolve);
+   } else {
+     resolve();
+   }
+ });
 }
 
-const insertStockData = function() {
-  StockChart.init()
-  .then(() => {
-    StockChart.create(stockData)
-      .then(() => db.close())
-      .catch((e) => {
-        console.log('\n \n \n THERE WAS AN ERROR IN THE DATABASE: \n \n \n', e.message);
-      })
-      .then(() => db.close());
-  })
+const generateStockData = () => {
+ const stream = fs.createWriteStream('data.csv');
+ for (let i = 0; i < 10000000; i++) {
+   writeTenMillionTimes(stream, JSON.stringify(generateData.generateData(i)) + '\n');
+ }
 };
 
-insertStockData();
+generateStockData();
+
+// const stockData = [];
+// for (let i = 0; i < 1000000; i++) {
+//   stockData.push(generateData.generateData(i));
+//   process.stdout.write(JSON.stringify(generateData.generateData(i)));
+// }
+
+// const insertStockData = function() {
+//   StockChart.init()
+//   .then(() => {
+//     StockChart.create(stockData)
+//       .then(() => db.close())
+//       .catch((e) => {
+//         console.log('\n \n \n THERE WAS AN ERROR IN THE DATABASE: \n \n \n', e.message);
+//       })
+//       .then(() => db.close());
+//   })
+// };
+
+// insertStockData();
